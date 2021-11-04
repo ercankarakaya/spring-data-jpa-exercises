acfefb511912395efb5062e366f1241759f67cb8
Ercan Karakaya <karakayaercan71@gmail.com>
2021-11-04 23:25:01 +0300

    Some editing.

--- src/main/java/com/ercan/aspect/LoggingAspect.java
+++ src/main/java/com/ercan/aspect/LoggingAspect.java
@@ -3,11 +3,10 @@ package com.ercan.aspect;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Aspect
 @Component
@@ -18,15 +17,33 @@ public class LoggingAspect {
     public void main() {
     }
 
-    @After("main()")
-    public void logMainMethod(ProceedingJoinPoint joinPoint) {
-        log.info("SPRING BOOT PROJECT STARTED :)");
+    @Around("execution(* com.ercan.controller.*.*(..))")
+    public Object logAllMethod(ProceedingJoinPoint joinPoint) throws Throwable {
+        if (log.isDebugEnabled()){
+            log.debug("Enter : {}.{}() with argument[s] = {} ",joinPoint.getSignature().getDeclaringTypeName(),
+                    joinPoint.getSignature().getName(), Arrays.toString(joinPoint.getArgs()));
+        }
+        try {
+            Object result = joinPoint.proceed();
+            if (log.isDebugEnabled()) {
+                log.debug("Exit: {}.{}() with result = {}", joinPoint.getSignature().getDeclaringTypeName(),
+                        joinPoint.getSignature().getName(), result);
+            }
+
+            return result;
+        }catch (IllegalArgumentException e){
+            log.error("Illegal argument: {} in {}.{}()", Arrays.toString(joinPoint.getArgs()),
+                    joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName());
+            throw e;
+        }
     }
+
 
     @AfterThrowing(value = "execution(* com.ercan.controller.TestController.*(..))", throwing = "ex")
     public void logAfterThrowing(JoinPoint joinPoint, Exception ex) {
         log.error("Target Method resulted into exception, message {} ", ex.getMessage());
 
     }
+
 
 }
