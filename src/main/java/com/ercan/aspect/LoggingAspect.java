e4cde5d9a3a761a35e50606f2e638568f9c4dc8d
Ercan Karakaya <karakayaercan71@gmail.com>
2021-11-04 22:19:51 +0300

    Some editing.

--- src/main/java/com/ercan/aspect/LoggingAspect.java
+++ src/main/java/com/ercan/aspect/LoggingAspect.java
@@ -2,8 +2,11 @@ package com.ercan.aspect;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -11,7 +14,16 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class LoggingAspect {
 
-    @AfterThrowing(value = "execution(* com.ercan.controller.TestController.*(..))",throwing = "ex")
+    @Pointcut("execution(* com.ercan.SpringDataJpaApplication.main(..))")
+    public void main() {
+    }
+
+    @After("main()")
+    public void logMainMethod(ProceedingJoinPoint joinPoint) {
+        log.info("SPRING BOOT PROJECT STARTED :)");
+    }
+
+    @AfterThrowing(value = "execution(* com.ercan.controller.TestController.*(..))", throwing = "ex")
     public void logAfterThrowing(JoinPoint joinPoint, Exception ex) {
         log.error("Target Method resulted into exception, message {} ", ex.getMessage());
 
