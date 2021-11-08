0b099803c4f3ce0e53832fd7216622ec2014bba0
Ercan Karakaya <karakayaercan71@gmail.com>
2021-11-08 12:23:17 +0300

    Some editing.

---
+++ src/main/java/com/ercan/service/specification/SearchCriteria.java
@@ -0,0 +1,15 @@
+package com.ercan.service.specification;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SearchCriteria {
+    private String key;
+    private String operation;
+    private Object value;
+
+}
